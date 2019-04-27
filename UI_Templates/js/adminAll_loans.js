cda9e851175c6b3b078b13a2cc9fcb5fe67d0093
rigoeffector <rigorene48@gmail.com>
2019-04-27 05:28:10 -0700

    Added search filter for status of loans

--- UI_Templates/js/adminAll_loans.js
+++ UI_Templates/js/adminAll_loans.js
@@ -18,6 +18,25 @@ function myFunction() {
   }
 
 
+  function myFilter(){
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput-status");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[3];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
+    }
+  }
+
 
   
     var openModelBtn = document.querySelectorAll("#app-detail");
